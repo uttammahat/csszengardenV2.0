cb996bb84df87a20878b8f3f4438179154d8a0c3
Uttam Mahat <lune.nuit100180@gmail.com>
2018-10-26 15:10:47 +0545

    version 2.0

---
+++ assets/js/functions.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    /*animation on windows load*/
+    $('body').animate({
+        opacity: 1
+    }, 'slow');
+
+
+    var pContainerHeight = $('.page-wrapper').height();
+
+
+    $(window).scroll(function() {
+        var fadeOne = $('.preamble'),
+            wh = $(window).height(),
+            dt = $(document).scrollTop(),
+            fadeOneView = wh - (fadeOne.offset().top - dt);
+        $('.preamble').each(function() {
+            var position = $(this).offset().top;
+            var height = $(this).height();
+            var topOfWindow = $(window).scrollTop();
+
+            if (position < topOfWindow + height && position + height > topOfWindow) {
+                fadeOne.css('opacity', 0 + Math.min(1 / wh * fadeOneView, 1));
+                console.log("topOfWindow" + topOfWindow, ",dt= " + dt, ",wh = " + wh, ",fadeOneView= " + fadeOneView);
+            }
+        });
+    });
+
+});
